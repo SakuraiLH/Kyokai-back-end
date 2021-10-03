0d6540ed0f55b23a85be28b944167ec3807f8dc8
SakuraiLH <hikarilh@foxmail.com>
2021-10-03 16:49:33 +0800

    include: Introduce JsonForCPP
    Signed-off-by: SakuraiLH <hikarilh@foxmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
+#include <cpr/cpr.h>
+
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
+
 int main()
 {
-	
+	cpr::Response r = cpr::Get(cpr::Url{"https://t.me/s/codeforces_official"});
+	json j;
+	std::cout << r.url << std::endl
+	       	  << r.status_code << std::endl
+		  << r.header["content-type"] << std::endl
+		  << r.text << std::endl;
 	return 0;
 }
